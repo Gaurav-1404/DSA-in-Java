241672a9deb0542623c15e4b50b1280c728cfac2
Gaurav <gaurav.g1783@gmail.com>
2026-08-05 23:58:03 +0530

    Hay This is my first file

---
+++ A_AllGood.java
@@ -0,0 +1,6 @@
+
+public class A_AllGood {
+    public static void main(String[] args) {
+        System.out.println("Good! Let's start a new journey. \n\t Code. \n\t Learn. \n\t Improve. \n\t Repeat.");
+    }
+}
