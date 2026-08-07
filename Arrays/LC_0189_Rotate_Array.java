6fa1c93bbfb7baa46653d484c3b194075aefae61
Gaurav <gaurav.g1783@gmail.com>
2026-08-07 11:17:18 +0530

    LC 189 Rotate Array by K

---
+++ Arrays/LC_0189_Rotate_Array.java
@@ -0,0 +1,51 @@
+package Arrays;
+/*
+========================================
+LeetCode : 189
+Problem  : Rotate Array
+
+Topic     : Arrays
+Subtopic  : Array Rotation
+Pattern   : Reverse Array
+
+Difficulty: Medium
+
+Time Complexity  : O(n)
+Space Complexity : O(1)
+
+Approach:
+1. Take k modulo n to handle cases where k > n.
+2. Reverse the entire array.
+3. Reverse the first k elements.
+4. Reverse the remaining (n-k) elements.
+5. The array gets rotated to the right by k steps.
+
+Key Learning:
+- Reverse Technique
+- In-place Rotation
+- Modulo Optimization
+
+========================================
+*/
+public class LC_0189_Rotate_Array {
+    public static void rotate(int[] arr, int k){
+        int n = arr.length;
+        k = k%n;
+        int[] F = new int[n];
+        for(int i=0; i<n; i++){
+            F[(i+k) % n] = arr[i];
+        }
+        for(int i=0; i<n; i++){
+            arr[i] = F[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+
+        rotate(arr, 4);
+        for(int n: arr){
+            System.out.print(n + ", ");
+        }
+    }
+}
