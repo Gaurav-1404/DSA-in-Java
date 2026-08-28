1f4ccd0e733e351be867ebb74069b3055cc618d0
Gaurav <gaurav.g1783@gmail.com>
2026-08-28 12:22:28 +0530

    LC 33 search in rotated sorted array

--- Arrays/LC_0033_Search_in_Rotated_Sorted_Array.java
+++ Arrays/LC_0033_Search_in_Rotated_Sorted_Array.java
@@ -76,6 +76,36 @@ public class LC_0033_Search_in_Rotated_Sorted_Array {
 
         return -1;
     }
+    // Both optimal
+    public static int insearch(int[] arr, int target){
+        int left = 0; 
+        int right = arr.length-1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(arr[left] <= arr[mid]){
+                if(arr[left] <= target && target <arr[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }else{
+                if(arr[mid] < target && target <= arr[right]){
+                    left = mid +1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         
